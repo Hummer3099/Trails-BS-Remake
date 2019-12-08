9b9a32b48d5426f7d0fbf287e90e3c5907ea2c88
Hummer3099 <abeedo_nedal@live.com>
2019-12-08 15:42:33 +0100

    Minor UnitController Modifications
    RegisterTile() doesn't work properly

--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -13,12 +13,38 @@ public class UnitController : MonoBehaviour
     public int moveRange;
     void Start()
     {
-        unit = new Unit { attack = attack, defense = defense, attackRange = attackRange, moveRange = moveRange };
+        InitializeUnit();
+        RegisterTile();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void InitializeUnit()
+    {
+        unit = new Unit { attack = attack, defense = defense, attackRange = attackRange, moveRange = moveRange };
+        if (Unit.gameObject.CompareTag("Party"))
+        {
+            unit.enemy = false;
+            Debug.Log(Unit.name + " is a party member.");
+        }
+        else if (Unit.gameObject.CompareTag("Enemy"))
+        {
+            unit.enemy = true;
+            Debug.Log(Unit.name + " is an enemy.");
+        }
+    }
+
+    void RegisterTile()
+    {
+        Tile [,] tiles = MapGenerator.GetInstance().GetTiles();
+        float offset = 4.5F;
+        int x = (int)(Unit.transform.localPosition.x + offset);
+        int y = (int)(Unit.transform.localPosition.z + offset);
+        tiles[x, y].unit = unit;
+        Debug.Log(Unit.name + " added at " + x + " " + y);
     }
 }
