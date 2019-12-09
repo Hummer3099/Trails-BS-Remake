584c8bf0a10bc3d22721eb8b7ca3dd0fbff6d150
s16717 <56193797+s16717@users.noreply.github.com>
2019-12-09 19:35:51 +0100

    ui

---
+++ Assets/Scripts/HPBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBar : MonoBehaviour
+{
+    public float currentHealth = 5;
+    public float maxHealth = 10;
+    public Image HealthBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateHPBar();
+    }
+    void UpdateHPBar()
+    {
+        float calcHP = currentHealth / maxHealth;
+        HealthBar.transform.localScale = new Vector3(Mathf.Clamp(calcHP, 0, 1), HealthBar.transform.transform.localScale.y, HealthBar.transform.transform.localScale.z);
+
+    }
+}
---
+++ Assets/Scripts/Result.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Result
+{
+    public Vector3 destination { get; set; }
+    public float distance { get; set; }
+    public int x { get; set; }
+    public int y { get; set; }
+}
